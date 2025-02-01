80b38d081da8c1e613e2dd1ab218684c6f2fc23e
Rikj000 <rik_helsen@hotmail.com>
2025-02-01 20:10:16 +0100

    ⚡️ Updated `NoStuckPals` - Build `v0.0.4` - Implemented `ModConfigMenu` support

--- NoStuckPals/Scripts/main.lua
+++ NoStuckPals/Scripts/main.lua
@@ -1,13 +1,40 @@
 -- Author: Rikj000
-
-local config = require("./config")
+local json = require('json')
+local configPath = './Mods/NoStuckPals/NoStuckPals.modconfig.json'
+local configTemplatePath = './Mods/NoStuckPals/NoStuckPals.template.json'
+local config
 
 -- Define debug logging helper function
-local logHeader = "[NoStuckPals] "
+local logHeader = '[NoStuckPals] '
 local function LogDebug(message)
-    if (config.LogDebugInfo == true) then
-        print(logHeader .. message .. "\n")
+    if (config == nil or config.LogDebugInfo.live == true) then
+        print(logHeader .. message .. '\n')
     end
+end
+
+-- Define config creation helper function
+local function CreateConfig()
+    local configFile = io.open(configPath, 'w')
+    local configTemplateFile = io.open(configTemplatePath, 'r')
+    local configTemplateString = configTemplateFile:read('*all')
+    configTemplateFile:close()
+    configFile:write(configTemplateString)
+    configFile:close()
+    LogDebug('Created NoStuckPals.modconfig.json!')
+end
+
+-- Define config loading helper function
+local function LoadConfig()
+    if (io.open(configPath, 'r') == nil) then
+        CreateConfig()
+    end
+
+    local configFile = io.open(configPath, 'r')
+    local configString = configFile:read('*all')
+    configFile:close()
+
+    config = json.decode(configString)
+    LogDebug('Loaded NoStuckPals.modconfig.json!')
 end
 
 -- Define scale helper function
@@ -15,17 +42,24 @@ local function ScalePal(character, scale)
     character:SetActorScale3D({ X = scale, Y = scale, Z = scale })
 end
 
--- Register early hook
-NotifyOnNewObject("/Script/Pal.PalCharacter", function(Character)
-    if (Character.StaticCharacterParameterComponent.IsPal == false or config.ScaleSize > 1) then
+-- Register config (re)load hook + Load the initial config
+LoadConfig()
+RegisterHook('/Script/Engine.PlayerController:ServerAcknowledgePossession', function(_)
+    LoadConfig()
+end)
+
+-- Register scaling hook
+NotifyOnNewObject('/Script/Pal.PalCharacter', function(Character)
+    if (Character.StaticCharacterParameterComponent.IsPal == false or config.ScaleSize.live > 1) then
         return
     end
 
-    ScalePal(Character, config.ScaleSize)
-    LogDebug("Early hook - Scaled pal (and collision capsule) to smaller size " .. tostring(config.ScaleSize) .. '!')
+    ScalePal(Character, config.ScaleSize.live)
+    LogDebug('Early hook - Scaled pal (and collision box) to smaller size '
+            .. tostring(config.ScaleSize.live) .. '!')
 
     ExecuteWithDelay(1, function()
         ScalePal(Character, 1)
-        LogDebug('Delayed hook - Scaled pal (not collision capsule) back to original size 1!')
+        LogDebug('Delayed hook - Scaled pal (not collision box) back to original size 1!')
     end)
 end)
