d88cb0d18db0cf11a377338bd2179b3f87119b57
Rikj000 <rik_helsen@hotmail.com>
2025-02-01 19:58:05 +0100

    ⚡️ Updated `NoStuckPals` - Build `v0.0.1`

--- NoStuckPals/Scripts/config.lua
+++ NoStuckPals/Scripts/config.lua
@@ -1,34 +1,15 @@
 local config = {}
 -- --------------- SETTINGS - Start -------------- --
 
--- HitBox only
-config.OnlyScaleHitBox = true
+-- Size to scale to (default size = 1.0)
+config.ScaleSize = 0.25
 
--- XL pals
-config.AffectsXL = true
-config.ScaleXL = 0.4
+-- Scale hit-box size only  = true
+-- Scale hit-box + pal size = false
+config.ScaleHitBoxOnly = false
 
--- L pals
-config.AffectsL = true
-config.ScaleL = 0.5
-
--- M pals
-config.AffectsM = true
-config.ScaleM = 0.6
-
--- S pals
-config.AffectsS = true
-config.ScaleS = 0.7
-
--- XS pals
-config.AffectsXS = false
-config.ScaleXS = 0.8
-
--- Rare pals
-config.AffectsRare = true
-config.ScaleRare = 0.5
-
--- Debugging
+-- Enable debug logging  = true
+-- Disable debug logging = false
 config.LogDebugInfo = true
 
 -- --------------- SETTINGS - End ---------------- --
--- NoStuckPals/Scripts/main.lua
+++ NoStuckPals/Scripts/main.lua
@@ -3,31 +3,6 @@
 local config = require("./config")
 local hooked = false
 
--- Initialize arrays for each pal type
-local configAffectsSize = {
-    [5] = config.AffectsXL,
-    [4] = config.AffectsL,
-    [3] = config.AffectsM,
-    [2] = config.AffectsS,
-    [1] = config.AffectsXS
-}
-
-local configScaleSize = {
-    [5] = config.ScaleXL,
-    [4] = config.ScaleL,
-    [3] = config.ScaleM,
-    [2] = config.ScaleS,
-    [1] = config.ScaleXS
-}
-
-local logScaleSize = {
-    [5] = "XL",
-    [4] = "L",
-    [3] = "M",
-    [2] = "S",
-    [1] = "XS"
-}
-
 -- Initialize debug logging helper functions
 local logHeader = "[NoStuckPals] "
 local function LogDebug(message)
@@ -36,37 +11,13 @@ local function LogDebug(message)
     end
 end
 
-local function LogScaleSize(character)
-    if (config.AffectsRare == true and character.StaticCharacterParameterComponent:IsRarePal()) then
-        return "Rare"
-    else
-        return logScaleSize[character.StaticCharacterParameterComponent.Size]
-    end
-end
-
 -- Define scale helper functions
 local function ShouldScalePal(character)
-    if (character.StaticCharacterParameterComponent.IsPal == false) then
-        return false
-    elseif (config.AffectsRare == true and character.StaticCharacterParameterComponent:IsRarePal()) then
-        return true
-    elseif (configAffectsSize[character.StaticCharacterParameterComponent.Size] == true) then
-        return true
-    else
-        return false
-    end
+    return character.StaticCharacterParameterComponent.IsPal == true
 end
 
 local function ScalePal(character, scale)
     character:SetActorScale3D({ X = scale, Y = scale, Z = scale })
-end
-
-local function ScaleSize(character)
-    if (config.AffectsRare == true and character.StaticCharacterParameterComponent:IsRarePal()) then
-        return config.ScaleRare
-    else
-        return configScaleSize[character.StaticCharacterParameterComponent.Size]
-    end
 end
 
 -- Register early hook - Scales to custom pal + hit-box size
@@ -74,10 +25,8 @@ RegisterHook("/Script/Engine.PlayerController:ClientRestart", function()
     if (not hooked) then
         NotifyOnNewObject("/Script/Pal.PalCharacter", function(character)
             if (ShouldScalePal(character) == true) then
-                local scale = ScaleSize(character)
-                local palType = LogScaleSize(character)
-                ScalePal(character, scale)
-                LogDebug("Scaled " .. palType .. " pal (and hit-box) to size " .. tostring(scale) .. '!')
+                ScalePal(character, config.ScaleSize)
+                LogDebug("Early hook - Scaled pal (and hit-box) to size " .. tostring(config.ScaleSize) .. '!')
             end
         end)
     end
@@ -87,9 +36,8 @@ end)
 -- Register late hook - Restores pal scale, keeps modified hit-box size, if enabled
 RegisterHook("/Script/Pal.PalCharacterParameterComponent:OnInitializedCharacter", function(Context, OwnerCharacter)
     local character = OwnerCharacter:get()
-    if (config.OnlyScaleHitBox == true and ShouldScalePal(character) == true) then
-        local palType = LogScaleSize(character)
+    if (config.ScaleHitBoxOnly == true and ShouldScalePal(character) == true) then
         ScalePal(character, 1)
-        LogDebug("Scaled " .. palType .. " pal (not hit-box) to size 1!")
+        LogDebug("Late hook - Scaled pal (not hit-box) to size 1!")
     end
 end)
